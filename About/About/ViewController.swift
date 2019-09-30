41d372f5d9888622f2df43006f3b3ee0fb875da2
jasmine <ys.loisel@gmail.com>
2019-09-30 23:19:06 +0900

    파일 위치 변경

---
+++ About/About/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About
+//
+//  Created by yunseo on 9/30/19.
+//  Copyright © 2019 aboutNext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
